1dd8fd7f79930ebb9d60744cfd7e8931cd36ff16
JCotton1123 <jcotton1123@gmail.com>
2014-01-19 13:39:06 -0500

    Update reset-org.sql

--- reset-org.sql
+++ reset-org.sql
@@ -1,3 +1,8 @@
+--
+-- A SQL script for resetting a demo account. It does not touch the IaaS provider settings or blueprints, roles, profiles, etc.
+-- WARNING: Prior to running this script, you should delete any active formations within the CP. This will ensure all devices and their corresponding dns entries are deleted. 
+--
+
 SET @org_id := 1;
 SET @user_name := 'mjuszczak';
 SET @user_first_name := 'Matt';
